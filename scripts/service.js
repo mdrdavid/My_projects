d162820ae01923504c125dc490d44c6c325ac91b
david matovu <davidmatovu88@gmail.com>
2021-09-14 22:36:38 +0300

    worked on fonts

--- scripts/service.js
+++ scripts/service.js
@@ -1,4 +1,4 @@
-function validateForm() {
+function validateForm(){
     if(document.LoginForm.email.value == "") {
       alert("Email must be provided");
       document.LoginForm.email.focus();
@@ -11,4 +11,24 @@ function validateForm() {
     else{
         return true;
     }
+    }
+
+
+
+    /*function validate(){
+  var mail = document.getElementById("text").value;
+// + indicates one or more occurance of a character
+//.(period) if used inside square braces escape with backslash(\.) 
+// ? indicates zero or one occurance
+  var regex = /^([a-zA-Z0-9\._]+)@([a-zA-Z0-9])+.)([a-z]+)(.[a-z])?$/ 
+  // $/ ends the expression
+
+  if(regex.text(mail)){
+    alert("Your Email ID is valid")
+    return true;
+  }
+  else{
+    alert("Incorrect Email ID")
+    return false;
+  }
     }
